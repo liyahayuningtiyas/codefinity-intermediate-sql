40dd946428c43bc0cc301883933d48da16692ded
liyahayuningtiyas <>
2026-01-30 04:01:14 +0000

    Challenge: Get Minimum Price By Category

---
+++ joining_tables/challenge_get_minimum_price_by_category/query.sql
@@ -0,0 +1,8 @@
+SELECT category.name AS category_name, 
+       MIN(price)      AS min_price
+FROM   category
+JOIN   product 
+  ON   category.id = product.category_id
+GROUP  BY category_name
+HAVING COUNT(product.name) > 5
+ORDER  BY category_name;
